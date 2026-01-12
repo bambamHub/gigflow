1aa192e2901b6f116c4a626bd43d5ed9b5815991
Bambam Kumar Gupta <bambamkumar30082003.ara@gmail.com>
2026-01-12 22:08:32 +0530

    gigsSlice.js

--- frontend/src/redux/slices/gigsSlice.js
+++ frontend/src/redux/slices/gigsSlice.js
@@ -1,16 +1,29 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
-import api from '../../services/api'
+import api from '../../services/api.js' 
 
-export const fetchGigs = createAsyncThunk('gigs/fetchGigs', async ({ search = '' } = {}) => {
-  const params = search ? `?search=${search}` : ''
-  const response = await api.get(`/gigs${params}`)
-  return response.data
-})
+export const createGig = createAsyncThunk(
+  'gigs/createGig',
+  async (gigData, { rejectWithValue }) => {
+    try {
+      const response = await api.post('/gigs', gigData)
+      return response.data
+    } catch (error) {
+      return rejectWithValue(error.response?.data || 'Failed to create gig')
+    }
+  }
+)
 
-export const createGig = createAsyncThunk('gigs/createGig', async (gigData) => {
-  const response = await api.post('/gigs', gigData)
-  return response.data
-})
+export const fetchGigs = createAsyncThunk(
+  'gigs/fetchGigs',
+  async (search = '', { rejectWithValue }) => {
+    try {
+      const response = await api.get(`/gigs${search ? `?search=${search}` : ''}`)
+      return response.data
+    } catch (error) {
+      return rejectWithValue(error.response?.data || 'Failed to fetch gigs')
+    }
+  }
+)
 
 const gigsSlice = createSlice({
   name: 'gigs',
