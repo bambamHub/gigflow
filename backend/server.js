5499750ebfd39d5d81335ffc664be63aa4fd4526
Bambam Kumar Gupta <bambamkumar30082003.ara@gmail.com>
2026-01-12 20:13:42 +0530

    server.js

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://gigflow-bambam.vercel.app',
   credentials: true
 }));
 app.use(express.json());
