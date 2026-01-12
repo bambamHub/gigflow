6c48d82fa3768fdcb11b52993a0915fcec105627
Bambam Kumar Gupta <bambamkumar30082003.ara@gmail.com>
2026-01-12 21:26:49 +0530

    api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,8 +2,20 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL + "/api",
-  withCredentials: true
+  withCredentials: true,
 });
 
-export default api;
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
 
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+export default api;
